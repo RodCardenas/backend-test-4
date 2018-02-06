7bbf9a56c2e87a36cbebc5f0d312a153fab06941
Rodrigo Cardenas <rodrigo.cardenas@rigminder.com>
2018-02-06 02:32:51 -0600

    Functional IVR menu when calling +1 (346) 214-5383. Call forwarding to Rodrigo's personal number working. Removed CSRF token to test twillio api and will need to be fixed.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,56 @@
+require 'twilio-ruby'
+
 class CallsController < ApplicationController
   def index
-    v = 'Hello World! Currently running version ' + Twilio::VERSION + ' of the twilio-ruby library.'
-    render plain: v
+    render plain: "Call Rodrigo @ +1 (346) 214-5383."
+  end
+
+  # POST ivr/welcome
+  def ivr_welcome
+    response = Twilio::TwiML::VoiceResponse.new
+    response.gather(input: 'dtmf', num_digits: '1', action: menu_path, method: 'get') do |gather|
+      gather.say('Please press 1 to call Rodrigo or 2 to leave a voicemail.')
+    end
+
+    render xml: response.to_s
+  end
+
+  # GET ivr/selection
+  def menu_selection
+    user_selection = params[:Digits]
+
+    case user_selection
+    when "1"
+      twiml_dial('+12818985445')
+    when "2"
+      twiml_say('Recording voicemails not implemented yet.', true)
+    else
+      @output = "Returning to the main menu."
+      twiml_say(@output)
+    end
+
+  end
+
+  private
+  def twiml_say(message, exit = false)
+    response = Twilio::TwiML::VoiceResponse.new do |r|
+      r.say(message, voice: 'woman', language: 'en')
+      if exit
+        r.say("Have a good day!")
+        r.hangup
+      else
+        r.redirect(welcome_path)
+      end
+    end
+
+    render xml: response.to_s
+  end
+
+  def twiml_dial(phone_number)
+    response = Twilio::TwiML::VoiceResponse.new do |r|
+      r.dial(number: phone_number)
+    end
+
+    render xml: response.to_s
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :calls
+  # Root of the app
   root 'calls#index'
+
+  # webhook for your Twilio number
+  match 'ivr/welcome' => 'calls#ivr_welcome', via: [:get, :post], as: 'welcome'
+
+  # callback for user entry
+  match 'ivr/selection' => 'calls#menu_selection', via: [:get, :post], as: 'menu'
 end
